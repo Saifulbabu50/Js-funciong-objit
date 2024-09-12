1117977cd5d107cd0b941953a89fc33fbabb6472
Saifulbabu50 <srmdsaifulsaif@gmail.com>
2024-09-13 02:01:24 +0600

    birire sloks

--- paramiter.js
+++ paramiter.js
@@ -1,4 +1,23 @@
-function darbye(){
-    console.log('mama ckagarat dan')
+// function darbye(manye){
+    // console.log('ato taka dicen',manye)
+    // console.log('mama ckagarat dan')
+    // var darbyeprice = 50;
+    // var quantity = darbyeprice / manye ;
+    // return quantity;
+
+// }
+//  var taka = 555;
+// darbye(taka);
+// var darbyes = darbye(taka);
+// console.log('eitin darbyes:', darbyes);
+
+
+function birire(manye){
+    var birireprice = 5;
+    var quantity =  manye /birireprice;
+    return quantity;
+
 }
-darbye(55);
+ var taka =50;
+ var birires = birire(taka);
+ console.log('soloka birires:',birires);
