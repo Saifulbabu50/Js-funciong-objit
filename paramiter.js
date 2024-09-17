64b81a3e7b0b385f099e7c5334afd82065280d53
Saifulbabu50 <srmdsaifulsaif@gmail.com>
2024-09-17 18:48:52 +0600

    nambrrs para miter

--- paramiter.js
+++ paramiter.js
@@ -21,3 +21,18 @@ function birire(manye){
  var taka =50;
  var birires = birire(taka);
  console.log('soloka birires:',birires);
+
+
+ function vesNambers(nambers){
+    let sum = 0;
+    for(const namber of nambers){
+        console.log(namber);
+        sum = sum + namber;
+    }
+    return sum;  
+
+ }
+ const nambs = [76, 65, 88, 56];
+ const sum = vesNambers(nambs);
+ 
+ console.log('vesNambers ',sum);
